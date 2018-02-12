99a8f5785dddf1c94a574212706410dbf9d9bef0
Votinje <jevgeni.votin@blacklightsoftware.com>
2018-02-12 15:34:19 +0000

    Edit git.ps1

--- git.ps1
+++ git.ps1
@@ -1,4 +1,31 @@
-﻿
+﻿#$dir = $(Get-Location).Path;
+
+#
 #git remote add origin https://jackbls:pass123@github.com/JackBLS/ExternalRepo.git
-git remote set-url origin https://jackbls:pass123@github.com/JackBLS/ExternalRepo.git
-git push -u  origin --all
+#git remote set-url origin https://jackbls:pass123@github.com/JackBLS/ExternalRepo.git
+#git push -u  origin --all
+
+#git clone git@github.com:whatever folder-name -r
+$dirPath = "C:\Users\votinje\source\repos\TestRepo\folder-name"
+Try
+	if((Test-Path -Path $dirPath) -ne $true){
+		New-Item -ItemType Directory -Force -Path $dirPath
+	}
+	cd  $dirPath
+	$directoryInfo = Get-ChildItem $dirPath | Measure-Object
+	if($directoryInfo.count -eq 0){
+		git clone https://jackbls:pass123@github.com/JackBLS/ExternalRepo.git $dirPath
+	}
+	else{
+		git pull 
+	}
+
+	git add .
+	git tag -l "v0.1"
+	git commit -m "test 01"
+	git push -u  origin --all
+}
+Catch
+{
+    Write-Host  $_.Exception.Message
+}
