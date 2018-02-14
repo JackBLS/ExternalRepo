dcc584be554d3e140f4ea140c1cf990cd168589d
tfsservice <tfsservice@blacklightsoftware.com>
2018-02-14 09:47:53 +0000

    test 01

---
+++ test/git.ps1
@@ -0,0 +1,71 @@
+﻿#$dir = $(Get-Location).Path;
+
+#
+#git remote add origin https://jackbls:pass123@github.com/JackBLS/ExternalRepo.git
+#git remote set-url origin https://jackbls:pass123@github.com/JackBLS/ExternalRepo.git
+#git push -u  origin --all
+
+#git clone git@github.com:whatever folder-name -r
+Try{
+
+	$dirPath = "C:\Git Mirror\GBG"
+
+	if((Test-Path -Path $dirPath) -ne $true){
+		New-Item -ItemType Directory -Force -Path $dirPath
+	}
+	cd  $dirPath
+	$directoryInfo = Get-ChildItem $dirPath | Measure-Object
+	if($directoryInfo.count -eq 0){
+		git clone https://jackbls:pass123@github.com/JackBLS/ExternalRepo.git $dirPath
+	}
+	else{
+		git fetch
+		git reset --hard
+	}
+
+	git add .
+	git tag -l "v0.1"
+	git commit -m "test 01"
+	git push -u  origin --all
+}
+Catch
+{
+    Write-Host  $_.Exception.Message
+}
+
+
+#########################################################
+$dirPath = "C:\Git Mirror\GBG"
+Try{
+	if((Test-Path -Path $dirPath) -ne $true){
+		New-Item -ItemType Directory -Force -Path $dirPath
+	}
+	cd  $dirPath
+	$directoryInfo = Get-ChildItem $dirPath | Measure-Object
+	if($directoryInfo.count -eq 0){
+		git clone https://jackbls:pass123@github.com/JackBLS/ExternalRepo.git $dirPath
+	}
+	else{
+		git fetch
+		git reset --hard
+	}
+}
+Catch
+{
+    Write-Host  $_.Exception.Message
+}
+
+
+#################################################################
+$dirPath = "C:\Git Mirror\GBG"
+try{
+	cd  $dirPath
+	git add .
+	git tag -l "v0.1"
+	git commit -m "test 01"
+	git push -u  origin --all
+}
+Catch
+{
+    Write-Host  $_.Exception.Message
+}
